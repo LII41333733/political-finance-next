bdbec7515c60b9836a10b27fef60042effd7c7b2
Mark Fisher <mfisher36@gmail.com>
2020-05-27 02:54:49 -0400

    route support

--- components/data/articles.js
+++ components/data/articles.js
@@ -99,7 +99,7 @@ export default [
         short: "As mentioned in the promotions for The Art of Political Finance Part I and II, the artists of the ...",
         title: `The \"Evolving Vise\" of the Command Economy`,
         thumbSrc: `/images/articles/vise-thumb.png`,
-        metaSrc: `images/articles/coronavirus-thumb.png`,
+        metaSrc: `images/articles/vise-thumb.png`,
         src: `/images/articles/vise.png`,
         date: "March 30, 2020",
         body: (<>
