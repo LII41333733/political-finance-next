87cf75946a7665fe6a101e8f7fc4e203aade81d4
Mark Fisher <mfisher36@gmail.com>
2020-06-13 10:31:40 -0400

    print this for me

--- pages/tools/timelines.js
+++ pages/tools/timelines.js
@@ -23,9 +23,6 @@ export default () => {
                         ? <>
                             <RenderTimelineCards
                                 onClick={setTimelineIndex} />
-                            <a target="_blank" href="https://store.printthisforme.com"><img className="printThis mt-5" src={"../images/printthis.jpg"} /></a>
-                            <p>is a full service online print and design company that specializes in full color offset and digital printing. <br /> Please feel free to pay their site a visit for any media printing inquiries.</p>
-                            <p>Timelines can be downloaded for wide format printing for the classroom or the wall at 300 dpi.</p>
                         </>
                         : <>
                             <BackButton
@@ -47,6 +44,9 @@ export default () => {
                                 alt={timelines[timelineIndex].alt} />
                         </>
                     }
+                    <a target="_blank" href="https://store.printthisforme.com"><img className="printThis mt-5" src={"../images/printthis.jpg"} /></a>
+                    <p>is a full service online print and design company that specializes in full color offset and digital printing. <br /> Please feel free to pay their site a visit for any media printing inquiries.</p>
+                    <p>Timelines can be downloaded for wide format printing for the classroom or the wall at 300 dpi.</p>
                 </Col>
             </Row>
         </ContainerWrapper>
