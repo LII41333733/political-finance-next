319eef9489f9210e56c1ab68e4fddcc2c79af871
Mark Fisher <mfisher36@gmail.com>
2020-07-09 04:16:26 -0400

    art6

--- components/data/articles.js
+++ components/data/articles.js
@@ -399,84 +399,92 @@ the general welfare.</em></p>
         date: `June 30, 2020`,
         body: (
             <>
+                <p>From the perspective of someone who has experienced 37 years in the 20<sup>st</sup>-century and 20 years in the 21<sup>st</sup>, the
+                challenges are mounting in this century. Many of them can be summed up with the application of the product
+                lifecycle. The <strong>Product Life Cycle Theory</strong> is an economic theory that was developed by Raymond Vernon in
+                response to the failure of the Heckscher-Ohlin model to explain the observed pattern of international trade. The
+                theory suggests that early in a product's life-cycle all the parts and labor associated with that product come from
+                the area where it was invented. After the product becomes adopted and used in the world markets, production
+                gradually moves away from the point of origin. In some situations, the product becomes an item that is imported
+by its original country of invention.<sup>i</sup></p>
+                <p>Some of you may be familiar with the graph of the product lifecycle. The first image shows how a product goes
+                through the stages from the perspective of manufacturing. The second shows the stages of the product by way
+                of consumption. We cannot marginalize the
+                complex factors of the current agitation, but
+                let’s assume that part of the frustration comes
+                from living in the urban cities which offer little
+                and dwindling COMMERCIAL opportunities
+                against the vacation of manufacturing and
+                related jobs to other parts of the globe as
+                indicated by this model. The model helps to
+                explain the evacuation of manufacturing that
+                took place in the United States in the last 50
+                years or so. Who is to blame? Is it China? Is it
+                the Republicans, who in modern times are the
+                party of big business? Is it President Trump,
+                who has campaigned on bringing those types of jobs back to America? Is it the Democrats, who claim to be the
+                party of the people while in the 1990s subjected Americans to the very “free trade” agreements under Bill Clinton
+                that helped create the condition? Is it the Confederates again, whose symbols are now under attack? Is it the
+                corporations that choose to offshore for cheap labor? Is it the Federal Government that allows these corporations
+                to offshore? Is it the state governments from where these corporations get their charters? We can point fingers
+                until doomsday. But in light of the public relations blitz during the COVID-19 pandemic and protests by Black
+                Lives Matter, Antifa, and other groups, many of these corporations have stated that, “we’re here to help.” If they
+                are sincere then the Nikes, the Walmarts, and the Amazons of the world that espouse superficial political
+                correctness should be transforming our rusty and toxic cities into productive centers again. This would be an
+                exercise of true political correctness to the sovereign. The questions are, do they have a sovereign, and do the
+                governors of these states and the mayors of these cities want the same? Are they willing to cede some tax
+                revenue, both corporate and personal in order to facilitate this process? Not to apologize for what should be
+                billed as a politically incorrect exodus, it was partially spurred on by high taxes due to high administration costs.
+                This is nothing new, it has been happening since the Civil War. After the Northern victory there were calls for
+                Civil Service reform aimed at radical Republicans who rode the four horsemen of civilization—a lawyer, a
+                politician, a policeman, and a tax collectorii— roughshod over a nation of “humble members of society” in what’s
+sometimes referred to as the Second American System.</p>
                 <p>
-                    Two months ago, I wrote an article entitled, The Coronavirus: Your Money or Your Life, discussing where
-                    officials and capitalists were weighing and questioning
-                    the severity of COVID-19 against the economic impact
-                    of isolation. I used the same cartoon by James Albert
-                    Wales because of the relevance of the battle that has
-                    been waging for over 150 years in the United States,
-                    but with a little variation. In the cartoon, Wales depicts
-                    monopoly (organized capital) against socialism
-                    (organized labor) with Puck seemingly pointing toward a
-                    blindfolded Lady Liberty trying to maintain the scales of
-                    justice.
+                    Let’s switch gears using the second graphic
+                    as a representation of the product lifecycle
+                    and apply it to education, which has been the
+                    bellows to more money as the solution to
+                    economic inequality. After World War II and
+                    the space race, much of the education
+                    systems and curriculum were designed
+                    around jobs surrounding the military
+                    industrial complex. It became the goal of
+                    most schools to prepare their students for
+                    college. But the ideal of higher education
+                    was not new. It too was proliferated with the
+                    Second American System. A system that was
+                    an expansion of federal power which usurped general Democratic Party opposition, ex parte with secession. As
+                    Dr. Eric Rauchway of the University of California, Davis, puts it, it was a highly ambitious set of laws, providing
+                    federal aid for the transcontinental railroad, for the state university system, for the settlement of the West by
+                    homesteaders, for a national currency, and for a protective tariff.<sup>iii</sup> This was a relatively radical program for
+                    expanding federal power considering the generally anti-federalist approach during the antebellum period. It
+                    was mercantilist, aimed to get farmers and ordinary citizens to buy into it with the Homestead Act and the
+                    establishment of state universities, and generally Democrats opposed it.iv Along with the state universities, many
+                    private universities were established in a period of great production and in support of the namesakes’ interests;
+
+                    Colgate University-William Colgate (1819), Duke University-James Buchanan Duke (1838), Wharton School-
+                    Joseph Wharton (1881), Stanford University-Leland Stanford (1885), Drexel University-Anthony Drexel (1891),
+
+                    et al. From an industrial age perspective, since the last half of the 20<sup>th</sup>-century, the schools have been flooded by
+                    the late majority and laggards. This is not a critique of the talent or intelligence of the population, but the
+                    population’s position related to the productive capacities that the university system once supported. The shift
+                    is now centered around administration and the healthcare industrial complex, of which there seems to be a crisis.
+                    The next question becomes, “is the university and credentialing system saturated and are universities applying
+                    the same marketing approach to sustain their product’s lifecycle?” Workers in the field will give you a different
+                    answer than recent college graduates and their helicopter parents.
                 </p>
                 <p>
-                    In the 21<sup>st</sup>-century political economy the belligerents
-                    may have changed a little. Monopoly is still organized
-                    capital, maybe even more consolidated than in the
-                    Gilded Age, but socialism, as it was named,
-                    represented organized labor. Today, our “labor” force is
-                    comprised more of administration, at the civil and
-                    municipal levels, and less of production and labor as
-                    many of those types of jobs have been exported since
-                    the 1990s. Jack Bogle, of Vanguard, had once
-                    summarized the evolution of the American economy as
-                    Agrarian, Industrial, Service, and Paper. We might add
-                    “Digital” or “Virtual” to that evolution. And so, our paper
-                    or digital economy has taken an enormous hit with an
-                    estimated 30 million newly unemployed, which brings
-                    us to a revelation. Since the authorities at the state and
-                    federal levels have mandated that only “essential”
-                    businesses stay open and employees report, it begs the
-                    question of what is considered essential and what is not.
+                    In regard to healthcare, in which there is a direct conflict between both capitalism, socialism, and care, we can
+                    apply the same model to pharmaceuticals. Does the litigation kick in after saturation? Here is a partial list of
+                    lawsuits against certain medications according to Enjuris.com and Wikipedia.
                 </p>
                 <p>
-                    If COVID-19 is as lethal and as contagious as advertised, then why are big-box stores like Home Depot, and
-                    Walmart permitted to stay open to the public, even with so-called distancing in place? Why are we receiving
-                    postal mail—most of it commercial junk mail? How safe are newspapers and those annoying flyers that are
-                    thrown on the driveway every week? The inconsistent policies have revealed the fluff in our economy and the
-                    superfluous capital that recessions correct, IF PERMITTED. We have had two major recessions now in this
-                    highly lauded 21<sup>st</sup>-century economy. Which brings us to another revelation. The talking heads, the State and
-                    the media, erroneously refer to the market as the economy in the context of opening and closing businesses,
-                    schools, and organizations. The obfuscation begins to clear up by analyzing the language. A state cannot open
-                    or close an economy. It can help or hurt an economy which has been the battle between conservatives and
-                    liberals since the United States was founded. The more accurate term, which pundits avoid, that describes the
-                    mandates is market, or free market. Yes, there are other organizations like educational institutions which have
-                    been closed, but officials and the mainstream media do not use harsh language like “we had to shut down the
-                    free market,” because without a free market you cannot have a free society.
-                </p>
-                <p>
-                    The belligerents in this recession appear to be Democratically-controlled states who have irresponsibly run
-                    with deficit budgets for decades and the Federal Government who, in conjunction with the Federal Reserve,
-                    can print money on demand. These practices are respectively known as liberal fiscal policy, at the state level, and liberal monetary policy at the federal level. If the Feds decide to bailout the states with no stipulations on
-                    budgetary constraints, then what we are witnessing is modern liberalism represented on both sides with
-                    taxpayers—current and future—footing the bill, not only by way of money but by way of time, freedom, and
-                    property. This is not what Americans are accustomed to hearing. We are accustomed to hearing that
-                    Republicans are conservative, and Democrats are liberal. This may be true with social issues, but
-                    “inflationeering”—liberal monetary and fiscal policy—has been leveraged by both parties in this century.
-                </p>
-                <p>
-                    To help us understand this conflict even further, it appears that the collective is actually an antagonist to “We
-                    the People.” In a classical sense, people and their individual liberty represented liberalism. In the modern
-                    sense, liberalism is more related to collectively-controlled mechanisms, collective capital and collective
-                    bargaining. In this context the people who have not found a musical chair succumb to the vise of collectivism
-                    (modern liberalism). Ironically, modern liberals and their public relations machine vehemently criticize classical
-                    liberals (individuals) who do not wear masks and who also want the free market and their small businesses to
-                    open so they can pay tribute to the modern liberal machine. This condition is more apparent in blue states that
-                    rely heavily on deficit spending and have blitzed us with public relation campaigns. Writing from the beautiful
-                    state of New Jersey, and it is a beautiful state, represents the quintessential case of collective capital from New
-                    York with collective bargaining from Trenton. A friend in Indiana, a red state running at a surplus, tells me that
-                    people in his state do not want a federal bailout nor need one. Another argument for the passage of balanced
-                    budget legislation.
-                </p>
-                <p>
-                    Ironically, I was a proponent of states’ rights in the plight against federal overreach that started after the Civil
-                    War, and still am. However, if states operate irresponsibly, it is apparent that they cede their rights to the Fed.
-                    COVID-19 has revealed many aspects about the lifestyle of 21<sup>st</sup>-century Americans and the economy. Maybe
-                    it’s time to obey the force that is compelling us to cut back on the overdone commercial chaos and modern
-                    liberalism.
+                    There are plenty of improvements to be made in this country and plenty of work to be done. Some of us can
+                    recognize it right outside our window. Where is the money and the will to get it done? If you read my article
+                    entitled, <a href={"https://www.billpacello.com/articles/6"}>“The Evolving Vise of the Command Economy,”</a> you can get an idea that both organizations, that of capital
+                    and that of labor have raised price levels to the detriment of the masses. Some of the pain has been disguised
+                    by cheap products from China but if tariffs and other wars continue to restrict trade then it might get more
+                    painful unless American corporations reinvest in their country of origin.
                 </p>
             </>
         ),
@@ -485,7 +493,18 @@ the general welfare.</em></p>
             <em className="textSize-4"><p><sup>i</sup> Figure 1 Wales, James Albert. The Proper Way. Puck—&quot;Come down both of you off your high horses and meet on equal ground.&quot; 1886.</p></em>
         ),
         footer: (
-            <p><sup>i</sup> Madrick, Jeff.“How the Invisible Hand Was Corrupted by Laissez- Faire Economics.” <em>Evonomics</em>, 3 May 2018</p>
+            <>
+                <p><sup>i</sup> Madrick, Jeff.“How the Invisible Hand Was Corrupted by Laissez- Faire Economics.” <em>Evonomics</em>, 3 May 2018</p>
+
+                <p><sup>i</sup> Hill, Charles(2007).International Business Competing in the Global Marketplace 6th ed.McGraw - Hill.pp. 168. ISBN 978-0 - 07 -
+            310255 - 9.</p>
+
+                <p><sup>ii</sup> “Escape from the Garden.“ Hell on Wheels.AMC. 2014.</p>
+
+                <p><sup>iii</sup> Rauchway, Eric, When and(to an extent) why did the parties switch places, May 20, 2010. <a href={"https://www.chronicle.com/blognetwork/edgeofthewest/2010/05/20/when-and-to-an-extent-why-did-the-parties-switch-places/"}>https://www.chronicle.com/blognetwork/edgeofthewest/2010/05/20/when-and-to-an-extent-why-did-the-parties-switch-places/</a>.
+</p>
+                <p><sup>iv</sup> Ibid.</p>
+            </>
         ),
     },
 
